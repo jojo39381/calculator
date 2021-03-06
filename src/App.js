d9dbd3d5d21d95684b8657ead6da713337056601
Joseph Yeh <josephycyeh@gmail.com>
2021-03-05 20:44:07 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
+
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
-import styled from 'styled-components'
+
 
 // rate style
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hourly from './Hourly'
 ReactDOM.render(
   <React.StrictMode>
     <App></App>
